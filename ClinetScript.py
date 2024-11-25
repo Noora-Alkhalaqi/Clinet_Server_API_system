d7a3a54b2c552baa370dd4af2e9a694bca2d9f67
Noora-Alkhalaqi <nooraalkhalaqi1@outlook.com>
2024-11-25 19:09:16 +0300

    fix the menue numbers

--- ClinetScript.py
+++ ClinetScript.py
@@ -34,32 +34,32 @@ while True:
         while True:
             print("-----------------------------")
             print("-Search headlines menu:")
-            print("1.1- Search for keywords will allow you to search in the news for a keyword in the news.")
-            print("1.2- Search by category will allow you to select the news by category.")
-            print("1.3- Search by country will allow you to select news by country")
-            print("1.4- List all new headlines will allow you to  select news with no specific preference.")
-            print("1.5- Back to the main menu will take you back to the main menu.")
+            print("1. Search for keywords will allow you to search in the news for a keyword in the news.")
+            print("2. Search by category will allow you to select the news by category.")
+            print("3. Search by country will allow you to select news by country")
+            print("4. List all new headlines will allow you to  select news with no specific preference.")
+            print("5. Back to the main menu will take you back to the main menu.")
 
             # Ask the user to enter the option from the Search headlines menu
             option = int(input("please Enter the number of the service: "))
-            if option == 1.1:
+            if option == 1:
                 message = input("Please enter the keyword: ")
                 send_message(cs, message)
 
-            elif option == 1.2:
+            elif option == 2:
                 print("1.Business 2.General 3.Health 4.Science 5.Sports 6.Technology")
                 message = input("From the list above please enter the categoty: ")
                 send_message(cs, message)
 
-            elif option == 1.3:
+            elif option == 3:
                 print("1.au 2.ca 3.jp 4.ae 5.sa 6.kr 7.us 8.ma")
                 message = input("From the list above please enter the country: ")
                 send_message(cs, message)
 
-            elif option == 1.4:
+            elif option == 4:
                 print("four")
 
-            elif option == 1.5:
+            elif option == 5:
                 print("Back to the main menu.")
                 break
             # This else is used to handle the error when we have a misentering of a number
@@ -70,32 +70,32 @@ while True:
     elif option == 2:
         print("-----------------------------")
         print("-List of Sources menu:")
-        print("2.1- Search by category will allow you to select the sources by category.")
-        print("2.2- Search by country will allow you to select sources by country")
-        print("2.3- Search by language will allow you to select sources language.")
-        print("2.4- List all will allow you to select sources with no specific preference")
-        print("2.5- Back to the main menu will take you back to the main menu.")
+        print("1. Search by category will allow you to select the sources by category.")
+        print("2. Search by country will allow you to select sources by country")
+        print("3. Search by language will allow you to select sources language.")
+        print("4. List all will allow you to select sources with no specific preference")
+        print("5. Back to the main menu will take you back to the main menu.")
 
         option = int(input("please Enter the number of the service: "))
-        if option == 2.1:
+        if option == 1:
             print("1.Business 2.General 3.Health 4.Science 5.Sports 6.Technology")
             message = input("From the list above please enter the categoty: ")
             send_message(cs, message)
 
-        elif option == 2.2:
+        elif option == 2:
             print("1.au 2.ca 3.jp 4.ae 5.sa 6.kr 7.us 8.ma")
             message = input("From the list above please enter the country: ")
             send_message(cs, message)
 
-        elif option == 2.3:
+        elif option == 3:
             print("1.ar 2.en")
             message = input("From the list above please enter the language: ")
             send_message(cs, message)
 
-        elif option == 2.4:
+        elif option == 4:
             print("four")
 
-        elif option == 2.5:
+        elif option == 5:
             print("five")
             break
 
@@ -105,11 +105,10 @@ while True:
 
     # This elif is used to quit the program 
     elif option == 3:
+        # Close the Socket
+        cs.close()
         break
 
     # This else is used to handle the error when we have a misentering of a number
     else:
         print("Option not on the list.")
-
-# Close the Socket
-cs.close()
